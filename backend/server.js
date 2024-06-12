e374595ffe0b976eb39d61f337be62dc787eba39
IT22025180 <it22025180@my.sliit.lk>
2024-06-13 01:29:15 +0530

    FC

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+dotenv.config();
+//rest
+
+const app = express();
+
+const uri = 'mongodb+srv://vanuja2003:2003vanuja@cluster0.xyfwuiv.mongodb.net/?retryWrites=true&w=majority';
+
+const connect = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to mongodb databse');
+    } catch (error) {
+        console.log('MongoDb error: ', error);
+    }
+};
+
+connect();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'Welcome to KD Aircon server',
+    });
+});
+
+app.use(express.json());
+
+//port
+const PORT = process.env.PORT || 4000;
+
+//run
+app.listen(PORT, () => {
+    console.log(`Server is Running on ${PORT}`);
+})
+
+// app.use('/api', router);
+// app.use('/api', routerB);
+// app.use('/api', routerP);
+// app.use('/api', routerR);
